5a75d0d3501c552c891262ccd4196ea40a3adb9f
AlfredoCorvi <al.cor.vi05@gmail.com>
2025-09-20 14:23:21 -0600

    Edit and Delete posts functions

--- app.py
+++ app.py
@@ -27,5 +27,30 @@ def add():
         return redirect(url_for('home'))
     return render_template('add.html')
 
+@app.route('/edit/<int:id>', methods=('GET', 'POST'))
+def edit(id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (id,)).fetchone()
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?',                     
+                      (title, content, id))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('home'))
+
+    conn.close()
+    return render_template('edit.html', post=post)
+
+@app.route('/delete/<int:id>', methods=('POST',))
+def delete(id):
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('home'))
+
 if __name__ == "__main__":
     app.run(debug=True)
